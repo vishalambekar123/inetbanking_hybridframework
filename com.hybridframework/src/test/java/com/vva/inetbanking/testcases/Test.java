887db6812598ece1a528aca2c8e38896719e47b7
vishalambekar123 <104249227+vishalambekar123@users.noreply.github.com>
2022-04-23 16:30:23 +0530

    Update Test.java

--- com.hybridframework/src/test/java/com/vva/inetbanking/testcases/Test.java
+++ com.hybridframework/src/test/java/com/vva/inetbanking/testcases/Test.java
@@ -4,6 +4,13 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("main method");
+		Test t = new Test();
+		t.test();
+	}
+	
+	public void test(){
+		System.out.println("instance test method");
+	
 	}
 
 }
